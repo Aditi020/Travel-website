89fa10bb80c827ed2470fab433a04f1347fe8850
Aditi <aditi.kumar.2.2.24@gmail.com>
2023-10-12 09:27:43 +0530

    Routing done correctly

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-import React from 'react'
-import  ReactDOM from 'react-dom/client'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import { Auth0Provider } from '@auth0/auth0-react';
 
-const root=ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App/>)
+ReactDOM.render(
+    <Auth0Provider
+        domain="dev-8p41wq2fwxclsyum.us.auth0.com"
+        clientId="XStcRH0Zoy6qkf91osuTsGy4GONKefQA"
+        authorizationParams={{
+            redirect_uri: window.location.origin
+        }}>
+        <App />
+    </Auth0Provider>,
+    document.getElementById('root')
+);
+
